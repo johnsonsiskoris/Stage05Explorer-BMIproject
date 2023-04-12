af2b3c5b1e622ba20a0080a377977eff1ce0b69d
Johnson Siskoris Junior <124916383+johnsonsiskoris@users.noreply.github.com>
2023-04-12 15:38:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+
+// variables
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+form.onsubmit = event => {
+    event.preventDefault ()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
